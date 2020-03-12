cc7db71886632f0888ad0094588f6d9e432000b6
Vitor Martins Cruz <vitormartinscruz@yahoo.com>
2020-03-12 15:30:15 -0300

    Adicionando botão de refresh que atualiza a data/hora

--- app/src/main/java/br/usp/ardhouse/activity/MainActivity.java
+++ app/src/main/java/br/usp/ardhouse/activity/MainActivity.java
@@ -9,12 +9,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import br.usp.R;
 import br.usp.ardhouse.controller.MainActivityController;
 
 public class MainActivity extends AppCompatActivity {
 
     private String nomeArduino;
+
+    private LocalDateTime ultimaAtualizacao;
+
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
@@ -23,18 +29,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         MainActivityController controller = new MainActivityController(MainActivity.this);
         nomeArduino = controller.lerArduinoAtual();
-        TextView exibicao = findViewById(R.id.arduinoAtual);
+        TextView exibicao = findViewById(R.id.lblArduinoAtual);
         if(nomeArduino != null) {
             exibicao.setText(exibicao.getText().toString() + "" + nomeArduino);
         }else {
             exibicao.setText(exibicao.getText().toString() + "Nenhum arduino selecionado");
         }
+
+        atualizarData(null);
+
+
     }
 
     // Método para levar usuário à tela de selecionar um novo Arduino
     public void selecionarArduino(View view){
         Intent intent = new Intent(this, SelecionarArduinoActivity.class);
         startActivity(intent);
+    }
+
+    // Ultima atualização de horário
+    public void atualizarData(View view) {
+        ultimaAtualizacao = LocalDateTime.now();
+        TextView exibirData = findViewById(R.id.lblData);
+        String formatoDeData = "dd/MM/yyyy HH:mm:ss";
+        DateTimeFormatter formatador = DateTimeFormatter.ofPattern(formatoDeData);
+        String dateFormated = ultimaAtualizacao.format(formatador);
+        exibirData.setText("");
+        exibirData.setText(exibirData.getText() + " " + dateFormated);
+
     }
 
 
