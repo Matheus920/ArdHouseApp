622911699af3a9a30720710e9ee57d784d7ddf03
Vitor Martins Cruz <vitormartinscruz@yahoo.com>
2020-03-20 00:48:26 -0300

    Adicionando ícone do aplicativo e removendo Title Bar.

--- app/src/main/java/br/usp/ardhouse/activity/MainActivity.java
+++ app/src/main/java/br/usp/ardhouse/activity/MainActivity.java
@@ -29,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
         controller = new MainActivityController(MainActivity.this);
         exibicao = findViewById(R.id.lblNomeArduino);
 
